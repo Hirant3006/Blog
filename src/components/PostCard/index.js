2bd33440909bf2737f42b69af62b970f2bff162c
15110091 <lychautrinha@gmail.com>
2020-01-28 16:26:30 +0700

    add content format and moment

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from "react";
 import "./index.scss";
+import moment from 'moment';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 
 const PostCard = ({ fields }) => {
   useEffect(() => {
     console.log(fields);
   }, [fields]);
 
+  const formatTime = (date) => moment(date).format('MMMM DD,YYYY HH:mm');
+  
+  const options = {
+    renderNode: {
+      // [BLOCKS.PARAGRAPH]: (node, children) => node.content.replace('\n', '<br/>')
+    },
+    renderText: text => text.split('\n').flatMap((text, i) => [i > 0 && <br key={i} />, text])
+  };
+  
   return (
     <div className="card">
       <b className="card-cat mb-10 regular">{fields.category}</b>
       <a className="card-title mb-10">{fields.title}</a>
+      <span className="card-date small mb-10">{formatTime(fields.createDate)}</span>
       <img
         className="mb-20"
         alt={fields.image.fields.title}
         src={fields.image.fields.file.url}
       ></img>
-      <span className="card-content">{fields.content.content[0].content[0].value}</span>
+      <div className="card-content">{documentToReactComponents(fields.content,options)}</div>
       {/* <a className="mt-20 small">đọc thêm></a> */}
       <div className="card-footer mt-40">
-        <span className="card-date">{fields.createDate}</span>
+        footer
       </div>
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,6 @@ import "./home.scss";
 import ListPost from "./ListPost";
 
 export default Home => {
-
-  //  const PostCard = contents.map((content,index) => {
-  //    return <PostCard props={content} />
-  //  })
-
   return (
     <div className="main">
       <ListPost />
