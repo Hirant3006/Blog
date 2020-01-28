818cdb6a07d96e9652e94757e15e37ca5bf2079f
15110091 <lychautrinha@gmail.com>
2020-01-29 00:22:35 +0700

    'update'

--- src/layouts/SideBar.js
+++ src/layouts/SideBar.js
@@ -13,8 +13,8 @@ const SideBar = () => {
           Đây là nơi chứa mấy thứ nhảm nhí của mình.
           <br /> <br />
           {/* <div style={{textAlign:'center'}}> */}
-          Indie Song - Classic Movies - Novel Books <br/>
-          Coding - UI/UX
+          Indie Song &nbsp;&nbsp;  Classic Movies &nbsp;&nbsp;  Novel Books <br/>
+          <div className="mt-5" style={{textAlign:'center'}}>Front-end Web &nbsp;&nbsp; UI/UX</div>
           {/* </div> */}
         </div>
         <div>
@@ -23,7 +23,7 @@ const SideBar = () => {
           Hiện tại ở đây vẫn chưa hoàn thiện mà cũng không có mấy content, nên nếu
           hứng thú với mấy thứ ở trên thì liên lạc với tớ ha :>
         </div>
-        <div className="social-links mt-30 mb-30">
+        <div className="social-links mt-20 mb-30">
           <a className="facebook" href="https://www.facebook.com/Hirantinsleep"><i className="fab fa-facebook-f"></i></a>
           <a className="linkedin" href="https://www.linkedin.com/in/nh%C3%A3-l%C3%BD-980879138/  "><i className="fab fa-linkedin"></i></a>
           <a className="instagram" href="https://www.instagram.com/_hirant_/"><i className="fab fa-instagram"></i></a>
