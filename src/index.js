d904dcf61a6fdd2203e4476855d3842d0eaed284
15110091 <lychautrinha@gmail.com>
2020-04-04 21:15:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './layouts/';
 import { BrowserRouter } from 'react-router-dom';
 import './fonts/BeVietNam.ttf';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
