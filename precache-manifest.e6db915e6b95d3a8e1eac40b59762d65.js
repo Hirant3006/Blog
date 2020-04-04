f45fc7df014ab58832bcb95f8264940109bae8aa
15110091 <lychautrinha@gmail.com>
2020-04-04 23:02:14 +0700

    Updates

---
+++ precache-manifest.e6db915e6b95d3a8e1eac40b59762d65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ae381a48834a219e016da996aa9a3",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "6f1d80e4ffcf7cdcd91c",
+    "url": "/Blog/static/css/main.23c5edb7.chunk.css"
+  },
+  {
+    "revision": "96843947d2653a91ffc3",
+    "url": "/Blog/static/js/2.d3f816cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Blog/static/js/2.d3f816cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f1d80e4ffcf7cdcd91c",
+    "url": "/Blog/static/js/main.fea55758.chunk.js"
+  },
+  {
+    "revision": "9ba2b7a2db74835c433a",
+    "url": "/Blog/static/js/runtime-main.295fd07b.js"
+  },
+  {
+    "revision": "bd27b3ec97d12655c7a2803cb6464924",
+    "url": "/Blog/static/media/BeVietNam.bd27b3ec.ttf"
+  },
+  {
+    "revision": "2f69cd7bd833e01a320df894d380eab5",
+    "url": "/Blog/static/media/img.2f69cd7b.jpg"
+  }
+]);
